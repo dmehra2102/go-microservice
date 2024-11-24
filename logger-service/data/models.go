eea1c668d66ec0941091c88a30358623887ba224
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-11-24 11:20:37 +0530

    feat: added some more method for mongodb opertaions

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -87,4 +89,65 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 	}
 
 	return logs, nil
+}
+
+func (l *LogEntry) GetOne(id string) (*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var entry LogEntry
+	err = collection.FindOne(ctx, bson.M{"_id": docId}).Decode(&entry)
+	if err != nil {
+		return nil, err
+	}
+
+	return &entry, nil
+}
+
+func (l *LogEntry) DropCollection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	if err := collection.Drop(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel() // Ensure that resources are cleaned up
+
+	collection := client.Database("logs").Collection("logs")
+
+	// Convert the string ID to a MongoDB ObjectID
+	docId, err := primitive.ObjectIDFromHex(l.ID)
+	if err != nil {
+		return nil, fmt.Errorf("invalid ID format: %w", err) // Wrap the error for better context
+	}
+
+	update := bson.D{
+		{Key: "$set", Value: bson.D{
+			{Key: "name", Value: l.Name},
+			{Key: "data", Value: l.Data},
+			{Key: "updated_at", Value: time.Now()},
+		}},
+	}
+
+	result, err := collection.UpdateOne(ctx, bson.M{"_id": docId}, update)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update document: %w", err)
+	}
+
+	return result, nil
 }
